44b7136b5ea60dc0d0bd03f1b845d612d73fe0aa
SRC0747 <srcsrk1998@gmail.com>
2021-08-14 20:33:09 +0530

    Welcome to Employee Wage problem Calculating wages of employees of multiple companies.

---
+++ src/com/empwagemulticompany/EmpWageMultiCompany.java
@@ -0,0 +1,8 @@
+package com.empwagemulticompany;
+
+public class EmpWageMultiCompany {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Employee Wage problem Calculating wages of employees of multiple companies.");
+    }
+}
